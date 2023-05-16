d1430f98cfb2917d564dce7cec6891c017ffa1e7
Angoti <angoti@gmail.com>
2023-05-16 16:41:07 -0300

    configuraões feitas

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-  return (<View><Text>Alô mundo!</Text></View>);
+  return (<View style={styles.container}><Text style={styles.texto}>Alô mundo!</Text></View>);
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  texto: { fontSize: 20, color: 'black' }
+});
